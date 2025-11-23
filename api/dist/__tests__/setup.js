5b0f1731df7657607390cc60792c00f168629400
Feiko van Dijk <feiko60@gmail.com>
2025-11-23 10:09:24 +0100

    Remove dist files

--- api/dist/__tests__/setup.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Test setup file
-// Global test configuration and utilities
-Object.defineProperty(exports, "__esModule", { value: true });
-// Mock environment variables for testing
-process.env.NODE_ENV = 'test';
-process.env.PORT = '3001';
